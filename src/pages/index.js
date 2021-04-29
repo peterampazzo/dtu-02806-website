5fa6c1490932d168f8d12f23e70a1e443d77d9b4
Pietro Rampazzo <pietro@rampazzo.co>
2021-04-29 17:49:28 +0200

    smaller area

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,23 @@ const IndexPage = () => {
   return (
     <div className="relative bg-gray-200 overflow-hidden">
       <div className="mx-6">
-      <div className="max-w-3xl mx-auto">
+        <div className="max-w-3xl mx-auto">
         <h3 className="text-md text-center mt-6 mb-8">02806 Social data analysis and visualization</h3>
         <h1 className="text-4xl md:text-6xl text-center font-extrabold mb-10 font-serif">
           We created a speaker rider because journalism didn’t have one
         </h1>
-        <h4 className="text-lg md:text-2xl text-center mb-8 m-auto w-10/12">
+        <div className="flex flex-col m-auto w-full md:w-4/6 justify-center">
+        <h4 className="text-lg md:text-2xl text-center mb-8 m-auto">
           Organizers, speakers, and attendees need to all work together to make
           events more inclusive.
         </h4>
-        <div className="border-gray-400 border-t-2 border-b-2 py-4 px-6 mb-8">
-          {/* <div className="flex"><img src="https://pbs.twimg.com/profile_images/1206618382713401345/jErU_sUR_400x400.jpg" className="rounded-full w-full	" /><p>Pietro Rampazzo</p></div> */}
+        <div className="border-gray-400 border-t-2 border-b-2 py-4 px-6 mb-12">
           <ul className="list-none flex text-center justify-between text-lg font-medium flex-wrap flex-col md:flex-row">
             <li>Silvia De Sojo</li>
             <li>Stefan Petrovic</li>
             <li>Pietro Rampazzo</li>
           </ul>
+        </div>
         </div>
         <main className="font-serif">
           <p>
