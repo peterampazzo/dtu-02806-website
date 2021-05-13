305f7a5d67e0755c0da5959eee3e0372950bb8bf
Pietro Rampazzo <pietro@rampazzo.co>
2021-05-13 23:32:03 +0200

    legend improvement

--- src/components/maps.js
+++ src/components/maps.js
@@ -7,11 +7,11 @@ const Maps = () => {
   return (
     <div className="my-4">
       <h1 className="text-center text-3xl mb-4">About your Neighborhood</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 pb-6">
         <Map property={"b_seguretat"} title="Neighborhood security" />
         <Map property={"b_joc"} title="Neighborhood palygrounds" />
       </div>
-      <div className="mt-8 p-4 flex justify-evenly align-center text-center">
+      <div className="mt-8 p-4 flex justify-evenly align-center text-center shadow-2xl rounded-lg bg-white">
         {legend.map((item, index) => (
           <div key={index} className="text-xl">
             <div
