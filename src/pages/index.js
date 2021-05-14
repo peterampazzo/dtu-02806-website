37a8e789b1821eb31b8b1c79af5e058af2b01f9a
sdesojo <72127992+sdesojo@users.noreply.github.com>
2021-05-14 20:21:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,18 +68,15 @@ const IndexPage = ({ data }) => {
             <MDXRenderer>{data.satifaction.body}</MDXRenderer>
           </Row>
           <p className="mt-8">
-            CONCLUSION - Nunc et libero est. Vestibulum fringilla, metus in
-            tempus suscipit, quam arcu condimentum nunc, nec tincidunt tortor
-            nisi fringilla erat. Nullam venenatis, nulla sit amet malesuada
-            tempor, nisl odio placerat dui, placerat scelerisque ante felis
-            congue nulla. Proin nec ornare ante. Donec sollicitudin nibh eu
-            tellus sodales, eu sollicitudin quam euismod. Nullam eget arcu nec
-            nunc auctor consequat sed sed nisl. Donec gravida risus in leo
-            vulputate sodales. Suspendisse nibh purus, vestibulum nec porta ut,
-            vehicula eget urna. Mauris eget imperdiet enim, quis ultrices eros.
-            Quisque blandit elementum dolor, in venenatis nibh iaculis et. Sed
-            tincidunt ut leo at vehicula. Duis tortor neque, mollis ac
-            condimentum et, interdum nec nulla.
+            Final Discussion
+            
+            This report was divided into three sections that aimed to unravel the effects that the demographic characteristics of a district may have on the children of Barcelona.
+            
+            The first step aimed to understand how the children perceive these characteristics over their own neighbourhood. Initially, their perception of security was studied, and it was possible to understand in which neighbourhoods the children felt less secure. However, it was interesting to compare it with their perception over a more positive element, being the availability of playgrounds.  Certain similarities were identified. The neighbourhoods with lower income being the ones perceived as less secure and with fewer playgrounds. However, this behaviour was not maintained in all instances. It was as well interesting to analyse the differences between districts on the accessibility to materials and their family typologies.
+
+            Having learned a bit more about the districts, it was interesting to evaluate the feelings of children. The vital optimism bias was appreciated since over 50% of kids responded that they felt  _always_ Happy and Energetic over the past two weeks. However, when looking at the differences between districts it was not directly clear how the behaviours changed. By performing a radar plot it was possible to aggregate all information together and identify that there were indeed differences when it came to negative feelings. Different ratios were identified for _Boredom_ and _Stress_ over the districts, but it was difficult to identify a clear pattern. 
+
+            Lastly, in order to define one measure that could be compared against different attributes, "life satisfaction" was evaluated. It was possible to detect clearer distinctions on satisfaction levels when evaluating different income-related attributes. However, other variables were as well contributing, as it was possible to see with kids having learning difficulties or even their age.  These effects made us wonder if a machine learning algorithm could help us identify how the different variables, we had available, contributed to the children satisfaction.  A K-Means clustering was performed but the results obtained were not completely satisfying. Nevertheless, our feeling is that other algorithms might be able to provide better results. Thus our next steps would be to perform more trials and continue our investigation towards, which characteristics contribute to the kids' life satisfaction. When we can answer the question, we would be able to identify which corrective actions could boost children's welfare.
           </p>
           <div className="mb-6">
             <Links />
